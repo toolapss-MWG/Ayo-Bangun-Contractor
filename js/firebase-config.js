07cf2024eabb45073f6415c28a7b6422bc38d266
toolapss-MWG <whimasnanjar4@gmail.com>
2026-09-03 00:31:17 +0700

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -8,12 +8,13 @@
 // ============================================
 
 const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY_KAMU",
-  authDomain: "ayo-bangun-pos.firebaseapp.com",
-  projectId: "ayo-bangun-pos",
-  storageBucket: "ayo-bangun-pos.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyBntT312d0m0VFSPkqiDVUomflUWzcKVB4",
+  authDomain: "ayobangun-contractor.firebaseapp.com",
+  projectId: "ayobangun-contractor",
+  storageBucket: "ayobangun-contractor.firebasestorage.app",
+  messagingSenderId: "1099399042051",
+  appId: "1:1099399042051:web:764118e26a6cc479956a36",
+  measurementId: "G-01F0KBG41D"
 };
 
 // Inisialisasi Firebase
